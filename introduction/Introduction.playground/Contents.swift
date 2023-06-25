3ef63a4abf39625ac7e447e3050463eb9e647ba5
Riccardo Fano <rcr.fano@gmail.com>
2023-06-25 08:36:49 +0200

    Add integers

--- introduction/Introduction.playground/Contents.swift
+++ introduction/Introduction.playground/Contents.swift
@@ -38,3 +38,29 @@ print(movie.uppercased())
 // .hasPrefix/Suffix return a boolean if the case sensitive string matches, they're nice to have
 print(movie.hasPrefix("a day"))
 print(movie.hasSuffix(".engineer"))
+
+let score = 10
+// TIP: Swift ignores underscores so you can add them to make the number more readable
+let reallyBig = 1_000_000
+
+// all the standard math operators
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let halvedScore = score / 2
+// you can't use ** to elevate to the power of a number, theres' pow() for that
+let squaredScore = score * score
+
+// there's all the math assignment operators too
+var counter = 20
+counter *= 2
+counter /= 10
+counter += 1
+counter -= 3
+// you can't use ++ to increment by one, use += 1 instead
+// I don't mind it, but I don't know if Swift uses simple for loop like (var i = 0; i < arr.count; i++), I doubt it.
+// counter++
+
+let number = 120
+// there's a method in the standard library for `number % 3 == 0`, nice
+print(number.isMultiple(of: 3))
