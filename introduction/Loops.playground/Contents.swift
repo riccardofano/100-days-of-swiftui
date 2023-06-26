40c01df8aeea74ec47c8100c0a176888c819eeac
Riccardo Fano <rcr.fano@gmail.com>
2023-06-26 11:25:31 +0200

    Add for loops

---
+++ introduction/Loops.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS", "visionOS"]
+
+// for in loops
+for os in platforms {
+    print("Swift works great on \(os)")
+}
+
+// for range loops
+// 0...5 = from 0 to and including 5
+// 0..<5 = from 0 until and excluding 5
+for i in 1...12 {
+    print("5 x \(i) = \(5 * i)")
+}
+
+// you can use _ to ignore a variable so you don't get an unused variable warning when you don't need it.
+for _ in 1...5 {
+    print("Hello")
+}
