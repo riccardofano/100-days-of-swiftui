31552d4c6fe9e9e9244a1895ab7dcf5858b45931
Riccardo Fano <rcr.fano@gmail.com>
2023-06-26 11:58:09 +0200

    Fix spacing on case statements

--- introduction/FizzBuzz.playground/Contents.swift
+++ introduction/FizzBuzz.playground/Contents.swift
@@ -4,11 +4,11 @@ for i in 1...100 {
     switch (i.isMultiple(of: 3), i.isMultiple(of: 5)) {
     case (true, true):
         print("FizzBuzz")
-    case(false, true):
+    case (false, true):
         print("Buzz")
-    case(true, false):
+    case (true, false):
         print("Fizz")
-    case(false, false):
+    case (false, false):
         print(i)
     }
 }
