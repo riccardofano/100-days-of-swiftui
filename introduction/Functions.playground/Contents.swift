75c8e8f2290ce9eb374b80c9599ca488365811fb
Riccardo Fano <rcr.fano@gmail.com>
2023-06-26 23:11:33 +0200

    Add tuples

--- introduction/Functions.playground/Contents.swift
+++ introduction/Functions.playground/Contents.swift
@@ -26,7 +26,7 @@ func rollDice() -> Int {
 
 // if the function only contians one line you can omit the return keyword
 func rollDice2() -> Int {
-    Int.random(1...6)
+    Int.random(in: 1...6)
 }
 
 // if you have not specified a return type you can use return to instantly break out of a function
@@ -36,3 +36,29 @@ func maybePrint(num: Int) {
     }
     print(num)
 }
+
+// tuples allows you to return multiple values from a function
+// their values have names in Swift which seems nice, it's one of the things I wish Rust had
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Riccardo", lastName: "Fano")
+}
+
+// you don't even need to say the name of the value when returning from a function, Swift is smart enough to know what their names are supposed to be
+func getUser2() -> (firstName: String, lastName: String) {
+    ("Taylor", "Swift")
+}
+
+// you can access a tuple's value with .property
+print(getUser().firstName)
+
+// tuples don't *have* to have named values
+func getUser3() -> (String, String) {
+    ("Taylor", "Swift")
+}
+// when values don't have names you can access them with .index
+print(getUser3().0)
+
+// you can destruct tuples too
+let (firstName, lastName) = getUser()
+// the variable name can be whatever you want and you can ignore a value with an underscore
+let (name, _) = getUser()
