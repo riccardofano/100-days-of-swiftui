d22167971621202f5fa836374a0c78206b27ed27
Riccardo Fano <rcr.fano@gmail.com>
2023-06-30 09:00:06 +0200

    Add Navigation bar title

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,19 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            // You can only have 10 sibling elements
-            // so if you want to have more than that you can group them
-            Section {
-                Text("Hello world")
+        NavigationView {
+            Form {
+                // You can only have 10 sibling elements
+                // so if you want to have more than that you can group them
+                Section {
+                    Text("Hello world")
+                }
+                Section {
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                }
             }
-            Section {
-                Text("Hello world")
-                Text("Hello world")
-                Text("Hello world")
-                Text("Hello world")
-                Text("Hello world")
-            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
