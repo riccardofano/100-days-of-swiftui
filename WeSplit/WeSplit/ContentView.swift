5c28fbcb8e75659e5c0231cbfe19ab4212dd7ce8
Riccardo Fano <rcr.fano@gmail.com>
2023-06-30 11:33:21 +0200

    Creating pickers

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -15,14 +15,28 @@ struct ContentView: View {
     @State private var tipPercentage = 20
 
     var body: some View {
-        Form {
-            Section {
-                TextField( "Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR") )
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField( "Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR") )
+                        .keyboardType(.decimalPad)
+                    
+                    // in the article this wants to create another page when you click on it
+                    // it only works if this is inside a NavigationView (which is why we added it)
+                    // BUT for me it's creating a popup and it didn't need a NavigationView to work, must be an iOS version difference
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                }
             }
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
-            }
+            // it seems counter intuitive but the navigation title is applied to the first child of the NavigationView
+            // instead of the NavigationView itself, it's there because the view can show many screens and those should be ones dictating the name
+            .navigationTitle("WeSplit")
         }
     }
 }
