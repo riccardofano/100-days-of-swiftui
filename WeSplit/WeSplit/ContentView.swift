104ad1d95cfd376ae5519c806adfc402d82e64ca
Riccardo Fano <rcr.fano@gmail.com>
2023-06-30 13:35:18 +0200

    Add focused state to hide the keyboard

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -13,6 +13,16 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        return grandTotal / peopleCount
+    }
 
     var body: some View {
         NavigationView {
@@ -20,6 +30,7 @@ struct ContentView: View {
                 Section {
                     TextField( "Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR") )
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     // in the article this wants to create another page when you click on it
                     // it only works if this is inside a NavigationView (which is why we added it)
@@ -46,12 +57,20 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
                 }
             }
             // it seems counter intuitive but the navigation title is applied to the first child of the NavigationView
             // instead of the NavigationView itself, it's there because the view can show many screens and those should be ones dictating the name
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
