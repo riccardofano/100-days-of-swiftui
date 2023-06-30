3f51588e000e5a378c4e35f6e498cd2f6f403538
Riccardo Fano <rcr.fano@gmail.com>
2023-06-30 11:13:52 +0200

    Read bill cost from user

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        Form {
+            Section {
+                TextField( "Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR") )
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+            }
+        }
     }
 }
 
