f175f348d7f94d65f58d391ec587503491e22fb3
Riccardo Fano <rcr.fano@gmail.com>
2023-07-01 11:13:47 +0200

    Init Converter app

---
+++ Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Riccardo Fano on 2023-07-01.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
