acff2e98541a2c4d318a3274856b5bb43c1499db
Riccardo Fano <rcr.fano@gmail.com>
2023-07-01 16:36:38 +0200

    Clear temperature conversion app challenge

--- Converter/Converter/ContentView.swift
+++ Converter/Converter/ContentView.swift
@@ -7,15 +7,89 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+extension UnitTemperature {
+    var name: String {
+        switch self {
+        case .celsius:
+            return "Celsius"
+        case .fahrenheit:
+            return "Fahrenheit"
+        default:
+            return "Kelvin"
         }
-        .padding()
+    }
+    
+    /// Kelvin is the base unit
+    private func toBaseUnit(_ value: Double, from: UnitTemperature) -> Double {
+        switch self {
+        case .celsius:
+            return value + 273.15
+        case .fahrenheit:
+            return (value + 459.67) * 5 / 9
+        default:
+            return value
+        }
+    }
+    
+    /// Kelvin is the base unit
+    private func fromBaseUnit(_ value: Double, to: UnitTemperature) -> Double {
+        switch to {
+        case .celsius:
+            return value - 273.15
+        case .fahrenheit:
+            return (value - 273.15) * 9 / 5 + 32
+        default:
+            return value
+        }
+    }
+    
+    func convert(_ value: Double, to: UnitTemperature) -> Double {
+        let normalized = toBaseUnit(value, from: self)
+        return fromBaseUnit(normalized, to: to)
+        
+    }
+}
+
+struct ContentView: View {
+    let temperatureUnits: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
+    
+    @State private var degrees: Double = 15.00
+    @State private var convertFrom = UnitTemperature.celsius
+    @State private var convertTo = UnitTemperature.fahrenheit
+    
+    private var formatStyle = Measurement<UnitTemperature>.FormatStyle(
+        width: .abbreviated,
+        numberFormatStyle: .number
+    )
+    
+    var convertedDegrees: Double {
+        convertFrom.convert(degrees, to: convertTo)
+    }
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Degrees", value: $degrees, format: .number)
+                
+                Picker("Convert from", selection: $convertFrom) {
+                    ForEach(temperatureUnits, id: \.self) {
+                        Text($0.name)
+                    }
+                }
+
+                Picker("Convert to", selection: $convertTo) {
+                    ForEach(temperatureUnits, id: \.self) {
+                        Text($0.name)
+                    }
+                }
+            }
+            
+            Section {
+                Text("\(convertedDegrees.formatted())\(convertTo.symbol)")
+            } header: {
+                Text("Result")
+            }
+        }
     }
 }
 
