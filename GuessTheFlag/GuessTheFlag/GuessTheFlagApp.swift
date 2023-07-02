dd2cf7f7a193346f75ece7eac24d0d31b7c55573
Riccardo Fano <rcr.fano@gmail.com>
2023-07-02 18:49:10 +0200

    Init Guess the flag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Riccardo Fano on 2023-07-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
