766256e5c2de493a5752103fda093d69c713053d
Riccardo Fano <rcr.fano@gmail.com>
2023-07-02 21:53:41 +0200

    Look at different kinds of gradients

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,25 +10,16 @@ import SwiftUI
 struct ContentView: View {
     // having 2 children as the top level elements of the body creates 2 screens
     var body: some View {
-        // this in css would be
-        // flex coloumn
-        VStack(alignment: .leading, spacing: 50) {
-            Text("Hello longer thing")
-            Text("World")
-        }.background(.red)
-        // flex row
-        HStack(spacing: 20) {
-            Text("Hello, world!")
-            Text("This is inside a stack")
+        ZStack {
+            LinearGradient(gradient: Gradient(stops: [
+                .init(color: .white, location: 0.45),
+                .init(color: .black, location: 0.55),
+            ]), startPoint: .top, endPoint: .bottom)
+            
+            RadialGradient(gradient: Gradient(colors: [.blue, .black.opacity(0.1)]), center: .center, startRadius: 20, endRadius: 200)
+            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center).opacity(0.5)
         }
-        // position absolute
-        // or grid in the same cell
-        ZStack() {
-            // Color.red.frame(width: 200, height: 100)
-            Color.red
-            Text("Hello, world!")
-            Text("This is inside a stack")
-        }.ignoresSafeArea()
+        
     }
 }
 
