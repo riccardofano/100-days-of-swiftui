e82e43f90bd23e49898432a8cf10109fbdbc6cc5
Riccardo Fano <rcr.fano@gmail.com>
2023-07-05 10:23:11 +0200

    Storing Views as properties

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,31 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var useRedBackground = false
+    
+    let motto1 = Text("Draco dormiens")
+    let motto2 = Text("nunquam titillandus").foregroundColor(.blue)
     
     var body: some View {
         VStack {
-            VStack {
-                Text("Gryffindor")
-                    .font(.footnote)
-                Text("Hufflepuff")
-                Text("Ravenclaw")
-                Text("Slytherin")
-            }
-            .font(.title)
-            
-            VStack {
-                Text("Gryffindor")
-                    // you can't undo a blur
-                    // because it's not an "environmental modifier" <- not actually a real thing, just a way to think about them
-                    .blur(radius: 0)
-                Text("Hufflepuff")
-                Text("Ravenclaw")
-                Text("Slytherin")
-            }
-            .blur(radius: 5)
+            motto1
+            motto2
         }
-    }}
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
