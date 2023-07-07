ef58c5b4d9b3f454321b94a0a7ada31e93521b5d
Riccardo Fano <rcr.fano@gmail.com>
2023-07-07 10:33:50 +0200

    Init Rock-paper-scissors app

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Riccardo Fano on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
