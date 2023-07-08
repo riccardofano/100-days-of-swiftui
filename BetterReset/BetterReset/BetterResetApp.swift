e97bd35810cb0ef14dbbefdc0b21ff3fe689dde7
Riccardo Fano <rcr.fano@gmail.com>
2023-07-08 09:53:00 +0200

    Init BetterRest app

---
+++ BetterReset/BetterReset/BetterResetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterResetApp.swift
+//  BetterReset
+//
+//  Created by Riccardo Fano on 2023-07-08.
+//
+
+import SwiftUI
+
+@main
+struct BetterResetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
