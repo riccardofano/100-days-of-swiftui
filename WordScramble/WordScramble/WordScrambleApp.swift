d9c5b6ddedac9c7f6d2129f6ee81992bcd6c6a0c
Riccardo Fano <rcr.fano@gmail.com>
2023-07-09 21:50:46 +0200

    Init Word Scramble app

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Riccardo Fano on 2023-07-09.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
