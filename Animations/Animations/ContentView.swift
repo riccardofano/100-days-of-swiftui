b8bf6db38ef4a2b1da41f6f5a5d5f09d5e55c761
Riccardo Fano <rcr.fano@gmail.com>
2023-07-12 13:12:07 +0200

    Learn how to customize animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -12,15 +12,29 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
+            // animationAmount += 1
         }
         .padding(50)
-        .background(.red)
+        .background(.orange)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        // .scaleEffect(animationAmount)
+        // .blur(radius: (animationAmount - 1) * 3)
+        // .animation(.default, value: animationAmount)
+        // .animation(.easeOut, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
