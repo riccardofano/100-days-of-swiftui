a9e0ba9fbab68152e873f826797ff1f0b1e528fb
Riccardo Fano <rcr.fano@gmail.com>
2023-07-12 10:39:24 +0200

    Init Animations app

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Riccardo Fano on 2023-07-12.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
