78e425eee423670887a0ebb316530a9aa4eb6170
Riccardo Fano <rcr.fano@gmail.com>
2023-07-12 13:19:40 +0200

    Binding animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,30 +11,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            // animationAmount += 1
+        print(animationAmount)
+        
+        return VStack {
+            Spacer()
+            Stepper("Step amount", value: $animationAmount.animation())
+            
+            Spacer()
+            
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            
+            Spacer()
         }
-        .padding(50)
-        .background(.orange)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        // .scaleEffect(animationAmount)
-        // .blur(radius: (animationAmount - 1) * 3)
-        // .animation(.default, value: animationAmount)
-        // .animation(.easeOut, value: animationAmount)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        .padding()
     }
 }
 
