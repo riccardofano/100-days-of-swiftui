0159cb51b4581865008ba0cfe614bc57020491a3
Riccardo Fano <rcr.fano@gmail.com>
2023-07-13 21:38:22 +0200

    Use transitions for showing and hiding views

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,50 +7,24 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    @State private var dragAmount = CGSize.zero;
-//
-//    var body: some View {
-//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-//            .frame(width: 300, height: 200)
-//            .clipShape(RoundedRectangle(cornerRadius: 10))
-//            .offset(dragAmount)
-//            .gesture(
-//                DragGesture()
-//                    .onChanged { dragAmount = $0.translation }
-//                    .onEnded { _ in
-//                        withAnimation(.spring()) {
-//                            dragAmount = .zero
-//                        }
-//                    }
-//            )
-//    }
-//}
-
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = true;
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
     }
 }
 
