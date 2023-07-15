d8bcb34885dd296d7d2f2031fe22a6c3dda42e4f
Riccardo Fano <rcr.fano@gmail.com>
2023-07-15 23:09:48 +0200

    Ask amount of questions / highest multiplication table

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var highestTable = 10
+    
+    let difficulties = [5, 10, 20]
+    @State private var maxQuestions = 5
+    
+    @State private var question = ""
+    @State private var chosenTable: Int = 2
+    @State private var chosenFactor: Int = 5
+    @State private var possibleAnswers: [Int] = []
+    
+    @State private var showingAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    
+    @State private var currentQuestion = 1
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Form {
+            Stepper(
+                "Highest table: \(highestTable.formatted())",
+                value: $highestTable,
+                in: 2...12, step: 1
+            )
+            
+            Picker("Number of questions",selection: $maxQuestions) {
+                ForEach(difficulties, id: \.self) {
+                    Text("\($0) questions")
+                }
+            }
+            .pickerStyle(.wheel)
+            .frame(maxHeight: 120)
+            
+            Button("Generate questions", action: askQuestion)
+            
+        }.onAppear(perform: askQuestion)
     }
+    
+    func askQuestion() {
+    }
+    
+    func chooseAnswer(_ answer: Int) {
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
