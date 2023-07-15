2ab408e1cb2f6ef5f8e8f14e3ca95265e6595989
Riccardo Fano <rcr.fano@gmail.com>
2023-07-15 22:23:26 +0200

    Init Edutainment challenge app

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Riccardo Fano on 2023-07-15.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
