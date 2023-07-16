441788c8199c325f4545c79632fe1a0609baec4a
Riccardo Fano <rcr.fano@gmail.com>
2023-07-16 22:40:47 +0200

    Learn how to store user preferences

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,51 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingSheet = false
-    
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                Button("Show sheet") {
-                    showingSheet.toggle()
-                }
-                .sheet(isPresented: $showingSheet) {
-                    SecondView(name: "Something")
-                }
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                Button("Add Number") {
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
-            }
-            .toolbar {
-                EditButton()
-            }
-        }
-    }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-}
+    @AppStorage("Tap") private var tapCount = 0
 
-struct SecondView: View {
-    let name: String
-    @Environment(\.dismiss) var dismiss
-    
     var body: some View {
-        Text("Second view with name: \(name)")
-        Button("Dismiss") {
-            dismiss()
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
         }
     }
 }
