4f2f2bfacb56bdf298e25cd86d1954ad4b4695fc
Riccardo Fano <rcr.fano@gmail.com>
2023-07-16 22:12:01 +0200

    Init iExpense app

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Riccardo Fano on 2023-07-16.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
