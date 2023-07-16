76f97256d23808b2e9610c757404c7bad3c510a5
Riccardo Fano <rcr.fano@gmail.com>
2023-07-16 22:33:53 +0200

    Check out the .onDelete method

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -10,13 +10,38 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingSheet = false
     
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
-        Button("Show sheet") {
-            showingSheet.toggle()
+        NavigationView {
+            VStack {
+                Button("Show sheet") {
+                    showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SecondView(name: "Something")
+                }
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Something")
-        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
