1e1b2043cc208db75c13830c7a011c69433391ba
Riccardo Fano <rcr.fano@gmail.com>
2023-07-16 22:45:28 +0200

    Encode complex data types

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
     @AppStorage("Tap") private var tapCount = 0
+    @State private var user = User(firstName: "Bobby", lastName: "Smith")
 
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
+        VStack {
+            Button("Tap count: \(tapCount)") {
+                tapCount += 1
+            }
+            Button("Save user") {
+                let encoder = JSONEncoder()
+                if let data = try? encoder.encode(user) {
+                    UserDefaults.standard.set(data, forKey: "User")
+                }
+            }
         }
     }
 }
