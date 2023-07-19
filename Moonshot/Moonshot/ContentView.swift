882c379edfea1cabdb65c83eb0233ba66e2c0d6b
Riccardo Fano <rcr.fano@gmail.com>
2023-07-19 22:07:51 +0200

    Learn of GeometryReader

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        GeometryReader { geo in
             Image("watercolor-floral-illustration")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8)
+                .frame(width: geo.size.width, height: geo.size.height)
+                .background(.red)
         }
-        .padding()
     }
 }
 
