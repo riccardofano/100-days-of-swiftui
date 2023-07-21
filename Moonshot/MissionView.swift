d2345f3817ade9c4945ccbf766ee64a0906485f6
Riccardo Fano <rcr.fano@gmail.com>
2023-07-21 22:40:24 +0200

    Add dividers

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -12,7 +12,7 @@ struct MissionView: View {
         let role: String
         let astronaut: Astronaut
     }
-
+    
     let mission: Mission
     let crew: [CrewMember]
     
@@ -35,6 +35,11 @@ struct MissionView: View {
                         .resizable()
                         .scaledToFit()
                         .frame(width: geo.size.width * 0.6)
+                        .padding(.top)
+                    
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundColor(.lightBackground)
                         .padding(.vertical)
                     
                     VStack(alignment: .leading) {
@@ -45,32 +50,45 @@ struct MissionView: View {
                     }
                     .padding(.horizontal)
                     
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(crew, id: \.role) { crewMember in
-                                NavigationLink {
-                                    Text("Astronaut details")
-                                } label: {
-                                    HStack {
-                                        Image(crewMember.astronaut.id)
-                                            .resizable()
-                                            .frame(width: 104, height: 72)
-                                            .clipShape(Capsule())
-                                            .overlay(
-                                                Capsule()
-                                                    .strokeBorder(.white, lineWidth: 1)
-                                            )
-
-                                        VStack(alignment: .leading) {
-                                            Text(crewMember.astronaut.name)
-                                                .foregroundColor(.white)
-                                                .font(.headline)
-                                            Text(crewMember.role)
-                                                .foregroundColor(.secondary)
-                                        }
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundColor(.lightBackground)
+                        .padding(.vertical)
+                    
+                    HStack {
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text("Astronaut details")
+                            } label: {
+                                HStack {
+                                    Image(crewMember.astronaut.id)
+                                        .resizable()
+                                        .frame(width: 104, height: 72)
+                                        .clipShape(Capsule())
+                                        .overlay(
+                                            Capsule()
+                                                .strokeBorder(.white, lineWidth: 1)
+                                        )
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text(crewMember.astronaut.name)
+                                            .foregroundColor(.white)
+                                            .font(.headline)
+                                        Text(crewMember.role)
+                                            .foregroundColor(.secondary)
                                     }
-                                    .padding(.horizontal)
                                 }
+                                .padding(.horizontal)
                             }
                         }
                     }
@@ -87,7 +105,7 @@ struct MissionView: View {
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
     static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-
+    
     static var previews: some View {
         MissionView(mission: missions[0], astronauts: astronauts)
             .preferredColorScheme(.dark)
