c1f8862ceca91c487f1b9cdcf71282445e5519c7
Riccardo Fano <rcr.fano@gmail.com>
2023-07-23 21:35:20 +0200

    Learn about Shapes

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
+        
+        VStack {
+            CustomTriangle()
+                .fill(.orange)
+                .frame(width: 300, height: 100)
+        
+            CustomArc(startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 229), clockwise: false)
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .bevel))
+                .frame(width: 89, height: 200)
         }
-        .stroke(.blue, style: StrokeStyle(lineWidth: 14, lineCap: .round, lineJoin: .round))
+        
+    }
+}
+
+struct CustomTriangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct CustomArc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle(degrees: 90)
+        let adjustedStart = startAngle - rotationAdjustment
+        let adjustedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: adjustedStart, endAngle: adjustedEnd, clockwise: clockwise)
+
+        return path
     }
 }
 
