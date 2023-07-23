ba6e4b2a39dafa7481f584568f5f3c11f93df75a
Riccardo Fano <rcr.fano@gmail.com>
2023-07-23 21:24:11 +0200

    Init Drawing app

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Riccardo Fano on 2023-07-23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
