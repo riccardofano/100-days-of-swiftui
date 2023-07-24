190801b0b23863734c636831915110334130c517
Riccardo Fano <rcr.fano@gmail.com>
2023-07-24 21:21:57 +0200

    Learn how to use CGAffineTransform

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,21 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+
     var body: some View {
-        
         VStack {
-            Circle()
-                .strokeBorder(.blue, lineWidth: 40)
-            
-            CustomTriangle()
-                .fill(.orange)
-                .frame(width: 300, height: 100)
-        
-            CustomArc(startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 229), clockwise: false)
-                .strokeBorder(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .bevel))
-                .frame(width: 89, height: 200)
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .stroke(.red, lineWidth: 1)
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
         }
-        
+    }
+}
+
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+
+            path.addPath(rotatedPetal)
+        }
+
+        return path
     }
 }
 
