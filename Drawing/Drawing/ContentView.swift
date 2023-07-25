484ef3ee9c9bb8798de9ffe4fadb5e23346c4c77
Riccardo Fano <rcr.fano@gmail.com>
2023-07-25 06:53:05 +0200

    Learn how to run code on Metal

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,86 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-
+    @State private var colorCycle = 0.0
+    
     var body: some View {
         VStack {
-            Text("Hello World")
-                .frame(width: 300, height: 300)
-                .border(ImagePaint(image: Image("clouds"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.2), width: 30)
+            ColorCyclingView(amount: colorCycle)
             
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
+            Slider(value: $colorCycle)
         }
+        .padding()
     }
 }
 
-struct Flower: Shape {
-    var petalOffset: Double = -20
-    var petalWidth: Double = 100
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-
-            path.addPath(rotatedPetal)
-        }
-
-        return path
-    }
-}
-
-struct CustomTriangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
-    }
-}
-
-struct CustomArc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
+struct ColorCyclingView: View {
+    var amount = 0.0
+    var steps = 100
     
-    var insetAmount = 0.0
-
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle(degrees: 90)
-        let adjustedStart = startAngle - rotationAdjustment
-        let adjustedEnd = endAngle - rotationAdjustment
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps, id: \.self) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
         
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: adjustedStart, endAngle: adjustedEnd, clockwise: clockwise)
-
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
+        func color(for value: Int, brightness: Double) -> Color {
+            var targetHue = Double(value) / Double(steps) + amount
+            
+            if targetHue > 1 {
+                targetHue -= 1;
+            }
+            
+            return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
