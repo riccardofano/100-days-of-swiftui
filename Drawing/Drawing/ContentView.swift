dd0b44d0b76199385e37188e111e67294beb3eba
Riccardo Fano <rcr.fano@gmail.com>
2023-07-25 21:42:13 +0200

    Animate simple shapes

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,37 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var amount = 0.5
+    @State private var amount = 20.0
     
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .fill(.red)
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(.green)
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(.blue)
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
+        Trapezoid(insetAmount: amount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    amount = Double.random(in: 0...200)
+                }
             }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
     }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    // the value *needs* to be called animatableData
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: animatableData, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - animatableData, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
