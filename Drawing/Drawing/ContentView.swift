5d65bd89b48831e2d0cc176a6df86c4cf61e043e
Riccardo Fano <rcr.fano@gmail.com>
2023-07-26 21:08:43 +0200

    Create arrow with animatable thickness

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,89 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rows = 4
-    @State private var cols = 4
-    
-    @State private var amount = 20.0
+    @State private var amount = 10.0
     
     var body: some View {
-        Checkerboard(rows: rows, columns: cols)
-            .onTapGesture {
-                withAnimation(.linear(duration: 2)) {
-                    rows = 8
-                    cols = 16
-                }
-            }
-        
-        Trapezoid(insetAmount: amount)
-            .frame(width: 200, height: 100)
+        Arrow()
+            .stroke(.red, style: StrokeStyle(lineWidth: amount, lineCap: .round))
+            .frame(width: 100, height: 200)
             .onTapGesture {
                 withAnimation {
-                    amount = Double.random(in: 0...200)
+                    amount = Double.random(in: 1...50)
                 }
             }
     }
 }
 
-struct Trapezoid: Shape {
-    var insetAmount: Double
-    // the value *needs* to be called animatableData
-    var animatableData: Double {
-        get { insetAmount }
-        set { insetAmount = newValue }
-    }
-
+struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
 
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: animatableData, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - animatableData, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        path.move(to: CGPoint(x: 0, y: rect.maxY / 4))
+        path.addLine(to: CGPoint(x: rect.maxX / 2, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY / 4))
+        path.move(to: CGPoint(x: rect.maxX / 2, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX / 2, y: rect.maxY))
 
         return path
    }
-}
-
-struct Checkerboard: Shape {
-    var rows: Int
-    var columns: Int
-    
-    var animatableData: AnimatablePair<Double, Double> {
-        get {
-            AnimatablePair(Double(rows), Double(columns))
-        }
-        
-        set {
-            rows = Int(newValue.first)
-            columns = Int(newValue.second)
-        }
-    }
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        // figure out how big each row/column needs to be
-        let rowSize = rect.height / Double(rows)
-        let columnSize = rect.width / Double(columns)
-
-        // loop over all rows and columns, making alternating squares colored
-        for row in 0..<rows {
-            for column in 0..<columns {
-                if (row + column).isMultiple(of: 2) {
-                    // this square should be colored; add a rectangle here
-                    let startX = columnSize * Double(column)
-                    let startY = rowSize * Double(row)
-
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
-                }
-            }
-        }
-
-        return path
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
