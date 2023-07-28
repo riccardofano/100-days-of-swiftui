a83e5e3ff28e68f161c3078f41da85b6749a98b5
Riccardo Fano <rcr.fano@gmail.com>
2023-07-28 20:38:23 +0200

    Init ActivityTracker challenge app

---
+++ ActivityTracker/ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Riccardo Fano on 2023-07-28.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
