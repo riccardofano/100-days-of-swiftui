55df3a86180686cc9b1d43c684e14850b8d0abd5
Riccardo Fano <rcr.fano@gmail.com>
2023-07-29 21:38:08 +0200

    Init CupcakeCorner app

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Riccardo Fano on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
