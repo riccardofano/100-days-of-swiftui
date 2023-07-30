e7bc9efebf5cf35bd303724808d0ef5ea4b528ac
Riccardo Fano <rcr.fano@gmail.com>
2023-07-30 22:14:36 +0200

    Make @Published properties conform to Codable

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    @Published var name = "Riccardo Fano"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+enum CodingKeys: CodingKey {
+    case name
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
