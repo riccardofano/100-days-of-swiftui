7cbf84c54893eae2999dcc7483fe810a0adae94c
Riccardo Fano <rcr.fano@gmail.com>
2023-07-31 21:49:17 +0200

    Take in basic order details

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,99 +7,58 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    @Published var name = "Riccardo Fano"
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
+    @Published var type = 0
+    @Published var quantity = 3
     
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
     }
-}
-
-enum CodingKeys: CodingKey {
-    case name
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
 }
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        username.count < 3 || !email.contains("@")
-    }
+    @StateObject var order = Order()
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Account created")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cupcake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cupcakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        
+                        Toggle("Add sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
                 }
             }
-            .disabled(disableForm)
-        }
-//        List {
-//            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-//                if let image = phase.image {
-//                    image
-//                        .resizable()
-//                        .scaledToFit()
-//                } else if phase.error != nil {
-//                    Text("There was an error loading the image")
-//                } else {
-//                    ProgressView()
-//                }
-//            }
-//            .frame(width: 100, height: 100)
-//
-//            ForEach(results, id: \.trackId) { result in
-//                VStack(alignment: .leading) {
-//                    Text(result.trackName)
-//                        .font(.headline)
-//                    Text(result.collectionName)
-//                }
-//
-//            }
-//        }
-//        .task {
-//            await loadData()
-//        }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
         }
     }
 }
