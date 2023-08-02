df4bf3f5fd3dfb90d5d258f77df340fb3bb88683
Riccardo Fano <rcr.fano@gmail.com>
2023-08-02 22:45:17 +0200

    Show an error message if the order request fails

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationTitle = ""
     @State private var confirmationMessage = ""
     @State private var showingConfirmation = false
     
@@ -34,7 +35,7 @@ struct CheckoutView: View {
                         await placeOrder()
                     }
                 }
-                .alert("Thank you", isPresented: $showingConfirmation) {
+                .alert(confirmationTitle, isPresented: $showingConfirmation) {
                     Button("Ok") {}
                 } message: {
                     Text(confirmationMessage)
@@ -55,17 +56,20 @@ struct CheckoutView: View {
         let url = URL(string: "https://reqres.in/api/cupcakes")!
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
+         request.httpMethod = "POST"
         
         do {
             let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
             
             let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationTitle = "Thank you"
             confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
-            showingConfirmation = true
         } catch {
+            confirmationTitle = "Oh no"
+            confirmationMessage = "Something went wrong while trying to place your order :("
             print("Checkout failed miserably.")
         }
+        showingConfirmation = true
     }
 }
 
