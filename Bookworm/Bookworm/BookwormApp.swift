af4eb78443c75e77608cd5f18294816c9e119090
Riccardo Fano <rcr.fano@gmail.com>
2023-08-03 22:56:40 +0200

    Init Bookworm app

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Riccardo Fano on 2023-08-03.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
