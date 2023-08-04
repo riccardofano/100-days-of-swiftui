be87ad53ba15f444720885b7ed0a41780b98d079
Riccardo Fano <rcr.fano@gmail.com>
2023-08-04 22:48:23 +0200

    Hook up CoreData

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -8,47 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rememberMe = false
-    
-    @AppStorage("notes") private var notes = ""
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    @Environment(\.managedObjectContext) var moc
     
     var body: some View {
         NavigationView {
             VStack {
-                TextEditor(text: $notes)
-                    .padding()
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(.gray))
+                List(students) { student in
+                        Text(student.name ?? "Unknown")
+                }
                 
-                PushButton(title: "Push me", isOn: $rememberMe)
-                Text(rememberMe ? "On" : "Off")
-            }
-            .navigationTitle("Notes")
-            .padding()
-        }
-    }
-}
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
 
-struct PushButton: View {
-    var title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+
+                    let student = Student(context: moc)
+                    student.id = UUID()
+                    student.name = "\(chosenFirstName) \(chosenLastName)"
+                    
+                    try? moc.save()
+                }
+            }
         }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController()
+    
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
     }
 }
---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Riccardo Fano on 2023-08-04.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
