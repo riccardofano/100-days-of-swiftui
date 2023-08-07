7a12375ecddcc9f6912971c545c1ecb66ff25eae
Riccardo Fano <rcr.fano@gmail.com>
2023-08-07 15:42:25 +0200

    Explicitly make struct conform to Hashable protocol

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(students, id: \.self) { student in
+            Text("\(student.name) = \(student.hashValue)")
         }
-        .padding()
     }
 }
 
