9f04f3618c5b92f3afd8290130ae5fef521bc689
Riccardo Fano <rcr.fano@gmail.com>
2023-08-07 15:29:30 +0200

    Init CodeDataProject app

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Riccardo Fano on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
