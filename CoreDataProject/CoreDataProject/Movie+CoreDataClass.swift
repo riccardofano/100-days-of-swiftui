c6e39e2624629fdb678fd8785b6414571dacebe7
Riccardo Fano <rcr.fano@gmail.com>
2023-08-07 20:14:46 +0200

    Create NSManagedObject subclasses

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Riccardo Fano on 2023-08-07.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
