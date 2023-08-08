077d199d5f32e3fdabf273f57413aaa813b07659
Riccardo Fano <rcr.fano@gmail.com>
2023-08-08 15:21:58 +0200

    Init FriendFace challenge app

---
+++ FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Riccardo Fano on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
