28513404d408aa7b33ec8f3af1585e684b6b5bca
Riccardo Fano <rcr.fano@gmail.com>
2023-08-08 11:55:58 +0200

    Use an enum to set the predicate filter

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -13,113 +13,38 @@ struct Student: Hashable {
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    
-    // %@ is used as a placeholder for variables
-//    static let predicate = NSPredicate(format: "universe == %@", "Star Trek")
-//    static let predicate = NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])
-//    static let predicate = NSPredicate(format: "name BEGINSWITH %@", "E")
-//    static let predicate = NSPredicate(format: "name BEGINSWITH[c] %@", "e")
-//    static let predicate = NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")
-//    static let predicate = NSPredicate(format: "NOT name BEGINSWITH[c] %@ AND universe == %@", "e", "Star Wars")
-//
-//    @FetchRequest(sortDescriptors: [], predicate: predicate) var ships: FetchedResults<Ship>
-    
-//    @State private var lastNameFilter = "A"
-    
-    @FetchRequest(sortDescriptors: []) var countries: FetchedResults<Country>
+    @State private var lastNameFilter = "A"
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(countries) { country in
-                    Section(country.wrappedName) {
-                        ForEach(country.candyList, id: \.self) { candy in
-                            Text(candy.wrappedName)
-                        }
-                    }
-                }
+            VStack {
+                FilteredList(keyFilter: "lastName", predicate: .beginsWith, valueFilter: lastNameFilter) { (singer: Singer) in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
             }
-            
-            Button("Add") {
-                let candy1 = Candy(context: moc)
-                candy1.name = "Mars"
-                candy1.origin = Country(context: moc)
-                candy1.origin?.shortName = "UK"
-                candy1.origin?.name = "United Kingdom"
 
-                let candy2 = Candy(context: moc)
-                candy2.name = "KitKat"
-                candy2.origin = Country(context: moc)
-                candy2.origin?.shortName = "UK"
-                candy2.origin?.name = "United Kingdom"
+            Button("Add examples") {
+                let taylor = Singer(context: moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
 
-                let candy3 = Candy(context: moc)
-                candy3.name = "Twix"
-                candy3.origin = Country(context: moc)
-                candy3.origin?.shortName = "UK"
-                candy3.origin?.name = "United Kingdom"
+                let ed = Singer(context: moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
 
-                let candy4 = Candy(context: moc)
-                candy4.name = "Toblerone"
-                candy4.origin = Country(context: moc)
-                candy4.origin?.shortName = "CH"
-                candy4.origin?.name = "Switzerland"
+                let adele = Singer(context: moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
 
                 try? moc.save()
             }
+
+            Button("Show A") {
+                lastNameFilter = "A"
+            }
+
+            Button("Show S") {
+                lastNameFilter = "S"
+            }
         }
-//        VStack {
-//            FilteredList(keyFilter: "lastName", valueFilter: lastNameFilter) { (singer: Singer) in
-//                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-//            }
-//
-//            Button("Add examples") {
-//                let taylor = Singer(context: moc)
-//                taylor.firstName = "Taylor"
-//                taylor.lastName = "Swift"
-//
-//                let ed = Singer(context: moc)
-//                ed.firstName = "Ed"
-//                ed.lastName = "Sheeran"
-//
-//                let adele = Singer(context: moc)
-//                adele.firstName = "Adele"
-//                adele.lastName = "Adkins"
-//
-//                try? moc.save()
-//            }
-//
-//            Button("Show A") {
-//                lastNameFilter = "A"
-//            }
-//
-//            Button("Show S") {
-//                lastNameFilter = "S"
-//            }
-//        }
-//        List(ships, id: \.self) { ship in
-//            Text(ship.name ?? "Unknown name")
-//        }
-//
-//        Button("Add Examples") {
-//            let ship1 = Ship(context: moc)
-//            ship1.name = "Enterprise"
-//            ship1.universe = "Star Trek"
-//
-//            let ship2 = Ship(context: moc)
-//            ship2.name = "Defiant"
-//            ship2.universe = "Star Trek"
-//
-//            let ship3 = Ship(context: moc)
-//            ship3.name = "Millennium Falcon"
-//            ship3.universe = "Star Wars"
-//
-//            let ship4 = Ship(context: moc)
-//            ship4.name = "Executor"
-//            ship4.universe = "Star Wars"
-//
-//            try? moc.save()
-//        }
     }
 }
 
--- CoreDataProject/CoreDataProject/FilteredList.swift
+++ CoreDataProject/CoreDataProject/FilteredList.swift
@@ -8,22 +8,40 @@
 import SwiftUI
 import CoreData
 
+enum SearchPredicate: String {
+    // [c] makes the search case insensitive
+    case beginsWith = "BEGINSWITH[c]"
+    case endsWith = "ENDSWITH[c]"
+    case equals = "==[c]"
+    
+}
+
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     let content: (T) -> Content
+    var query = ""
+    
     @FetchRequest var fetchRequest: FetchedResults<T>
     
-    init(keyFilter: String, valueFilter: String, @ViewBuilder content: @escaping (T) -> Content) {
+    init(keyFilter: String,
+        predicate: SearchPredicate,
+        valueFilter: String,
+        @ViewBuilder content: @escaping (T) -> Content
+   ) {
         _fetchRequest = FetchRequest<T>(
             sortDescriptors: [],
             // %K is how you specify a k, it's not surrounded by quotes like %@
-            predicate: NSPredicate(format: "%K BEGINSWITH %@", keyFilter, valueFilter)
+            predicate: NSPredicate(format: "%K \(predicate.rawValue) %@", keyFilter, valueFilter)
         )
         self.content = content
+        self.query = "\(keyFilter) \(predicate.rawValue) '\(valueFilter)'"
     }
     
     var body: some View {
-        List(fetchRequest, id: \.self) { entity in
-            self.content(entity)
+        VStack {
+            Text(query)
+            List(fetchRequest, id: \.self) { entity in
+                self.content(entity)
+            }
         }
     }
 }
