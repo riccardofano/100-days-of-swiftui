c591a5c4dab3fe8df6c89a760241b855bb5faff5
Riccardo Fano <rcr.fano@gmail.com>
2023-08-10 10:59:27 +0200

    Create a confirmationDialog for the first time

--- InstaFilters/InstaFilters/ContentView.swift
+++ InstaFilters/InstaFilters/ContentView.swift
@@ -7,28 +7,50 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    @State private var blurAmount = 0.0 {
+//        didSet {
+//            print("Amount changed, now it's \(blurAmount)")
+//        }
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            Text("Hello, World!")
+//                .blur(radius: blurAmount)
+//
+//            Slider(value: $blurAmount, in: 0...20)
+//                .onChange(of: blurAmount) {
+//                    print("Slider changed blurAmount, it's now \($0)")
+//                }
+//
+//            Button("Random Blur") {
+//                blurAmount = Double.random(in: 0...20)
+//            }
+//        }
+//        .padding()
+//    }
+//}
+
 struct ContentView: View {
-    @State private var blurAmount = 0.0 {
-        didSet {
-            print("Amount changed, now it's \(blurAmount)")
-        }
-    }
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.yellow
 
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-
-            Slider(value: $blurAmount, in: 0...20)
-                .onChange(of: blurAmount) {
-                    print("Slider changed blurAmount, it's now \($0)")
-                }
-
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
             }
-        }
-        .padding()
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
+            }
     }
 }
 
