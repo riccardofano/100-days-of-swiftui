7221deea12dcbe29133e35eb8ae6ed86d67771bb
Riccardo Fano <rcr.fano@gmail.com>
2023-08-10 10:32:52 +0200

    Init InstaFilters app

---
+++ InstaFilters/InstaFilters/InstaFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFiltersApp.swift
+//  InstaFilters
+//
+//  Created by Riccardo Fano on 2023-08-10.
+//
+
+import SwiftUI
+
+@main
+struct InstaFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
