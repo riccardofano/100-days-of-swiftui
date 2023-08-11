f1eebc24d858afd281ae7a0ff285d719a16af5fd
Riccardo Fano <rcr.fano@gmail.com>
2023-08-11 15:12:01 +0200

    Get the basic layout going

--- InstaFilters/InstaFilters/ContentView.swift
+++ InstaFilters/InstaFilters/ContentView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    
+    @State private var intensity: Double = 1
+    
     var body: some View {
-        VStack {
-            Text("Hello")
+        NavigationView {
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerSize: CGSize(width: 5, height: 5))
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture { }
+                
+                HStack {
+                    Text("Filter intesity: \(intensity.formatted())")
+                    Slider(value: $intensity, in: 0...1)
+                }
+                
+                HStack {
+                    Button("Change filter") {}
+                    Spacer()
+                    Button("Save", action: save)
+                }
+            }
+            .navigationTitle("InstaFilters")
+            .padding()
         }
+    }
+    
+    func save() {
+        
     }
 }
 
