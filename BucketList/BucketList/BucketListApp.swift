eda55c3dcc5bb6f8c9667980b4d6ebcabc97e2db
Riccardo Fano <rcr.fano@gmail.com>
2023-08-13 17:42:02 +0200

    Init BucketList app

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Riccardo Fano on 2023-08-13.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
