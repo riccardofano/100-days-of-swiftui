616eca08dae99887e4968fddfb5ccda25d6791ab
Riccardo Fano <rcr.fano@gmail.com>
2023-08-14 20:39:29 +0200

    Integrate MapKit into the project

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct User: Identifiable, Comparable {
     static func < (lhs: User, rhs: User) -> Bool {
@@ -39,6 +40,12 @@ struct FailedView: View {
     }
 }
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
     let users = [
         User(firstName: "Arnold", lastName: "Rimmer"),
@@ -46,22 +53,43 @@ struct ContentView: View {
         User(firstName: "David", lastName: "Lister"),
     ].sorted()
     
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     @State private var loadingState = LoadingState.failed
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
     var body: some View {
-        VStack {
-            if loadingState == .loading {
-                LoadingView()
-            } else if loadingState == .success {
-                SuccessView()
-            } else if loadingState == .failed {
-                FailedView()
-            }
-            
-            Text("Hello World")
-                .onTapGesture {
-                    FileManager().writeToDocumentsFile(filename: "message.txt", message: "Dude where's my car?")
+        NavigationView {
+            VStack {
+                Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        NavigationLink {
+                            Text(location.name)
+                        } label: {
+                            Circle()
+                                .stroke(.red, lineWidth: 3)
+                                .frame(width: 44, height: 44)
+                        }
+                    }
                 }
+                
+                if loadingState == .loading {
+                    LoadingView()
+                } else if loadingState == .success {
+                    SuccessView()
+                } else if loadingState == .failed {
+                    FailedView()
+                }
+                
+                Text("Hello World")
+                    .onTapGesture {
+                        FileManager().writeToDocumentsFile(filename: "message.txt", message: "Dude where's my car?")
+                    }
+            }
+            .navigationTitle("Anor Londo Explorer")
         }
     }
     
