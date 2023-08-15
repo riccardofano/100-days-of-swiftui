5e71ce9233a5eea3060e82303698248512ed4114
Riccardo Fano <rcr.fano@gmail.com>
2023-08-15 19:17:31 +0200

    Allow selecting and editing annotations

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -11,6 +11,7 @@ import MapKit
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 50, longitudeDelta: 50))
     @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
     
     var body: some View {
         ZStack {
@@ -25,6 +26,10 @@ struct ContentView: View {
                             .clipShape(Circle())
 
                         Text(location.name)
+                            .fixedSize()
+                    }
+                    .onTapGesture {
+                        selectedPlace = location
                     }
                 }
             }
@@ -50,6 +55,13 @@ struct ContentView: View {
                             .clipShape(Circle())
                             .padding(.trailing)
                     }
+                }
+            }
+        }
+        .sheet(item: $selectedPlace) { place in
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
                 }
             }
         }
