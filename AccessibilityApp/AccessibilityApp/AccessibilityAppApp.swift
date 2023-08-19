3e6cc36aab5286ad7c908e25ee44034264bf2847
Riccardo Fano <rcr.fano@gmail.com>
2023-08-19 22:21:33 +0200

    Init Accessibility app

---
+++ AccessibilityApp/AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Riccardo Fano on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
