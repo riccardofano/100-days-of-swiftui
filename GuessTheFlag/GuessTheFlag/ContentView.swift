88c9c7b8b09ec480941828e2dfe71f34a9377c7d
Riccardo Fano <rcr.fano@gmail.com>
2023-08-20 21:30:10 +0200

    Update GuessTheFlag to not spoil the solution when using VoiceOver

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,6 +10,19 @@ import SwiftUI
 struct ContentView: View {
     private let totalQuestions = 8;
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
@@ -76,6 +89,7 @@ struct ContentView: View {
                                     .opacity(tappedIndex == nil || tappedIndex == index ? 1 : 0.25)
                                     .scaleEffect(tappedIndex == nil || tappedIndex == index ? 1 : 0.75)
                                     .animation(.default, value: rotationAmount)
+                                    .accessibilityLabel(labels[countries[index], default: "Unknown flag"])
                             }
                             .alert(scoreTitle, isPresented: $showingScore) {
                                 Button("Continue", action: askQuestion)
