10cd621818aa0a1c152392185ebfaf8cb47ccb1a
Riccardo Fano <rcr.fano@gmail.com>
2023-08-23 21:10:03 +0200

    Init ForgetMeNot challenge app

---
+++ ForgetMeNot/ForgetMeNot/ForgetMeNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgetMeNotApp.swift
+//  ForgetMeNot
+//
+//  Created by Riccardo Fano on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct ForgetMeNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
