b921171cfc2e55054ae0a9b9a44eed122cadc721
Riccardo Fano <rcr.fano@gmail.com>
2023-08-28 14:05:06 +0200

    Create a location fetcher to import the current location

---
+++ ForgetMeNot/ForgetMeNot/LocationFetcher.swift
@@ -0,0 +1,27 @@
+//
+//  LocationFetcher.swift
+//  ForgetMeNot
+//
+//  Created by Riccardo Fano on 2023-08-28.
+//
+
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
