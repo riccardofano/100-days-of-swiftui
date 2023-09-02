3ef09fd59c4a9808967183156429bd3ed5ac3d27
Riccardo Fano <rcr.fano@gmail.com>
2023-09-02 11:17:12 +0200

    Share env object with other views

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @StateObject private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            EditView().environmentObject(user)
+            DisplayView().environmentObject(user)
+        }.padding()
+    }
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        TextField("Name", text: $user.name)
+            .padding()
+            .background(.gray)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        Text(user.name)
     }
 }
 
