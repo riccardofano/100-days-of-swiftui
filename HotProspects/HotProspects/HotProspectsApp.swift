6399eb800bc4675c595df579e0dadd05e62774da
Riccardo Fano <rcr.fano@gmail.com>
2023-09-02 11:14:15 +0200

    Init HotProspects app

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Riccardo Fano on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
