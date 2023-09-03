a728107b64ef1e5796c9f77b0666862f49e9cba9
Riccardo Fano <rcr.fano@gmail.com>
2023-09-03 15:45:18 +0200

    Create a basic tab layout

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -10,8 +10,23 @@ import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello")
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
         }
     }
 }
---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,40 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Riccardo Fano on 2023-09-03.
+//
+
+import SwiftUI
+
+enum FilterType {
+    case none, contacted, uncontacted
+}
+
+struct ProspectsView: View {
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello, World")
+                .navigationTitle(title)
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
