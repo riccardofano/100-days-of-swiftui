07fdea13a6442139ce542ef89e175dd6a6fc2565
Riccardo Fano <rcr.fano@gmail.com>
2023-09-03 15:36:44 +0200

    Reset app to original state

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,46 +7,11 @@
 
 import SwiftUI
 import UserNotifications
-import SamplePackage
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-    
-    let arr = ["Hello", "Hi", "Bye", "Goodbye"]
-    var random: [String] {
-        arr.random(2)
-    }
-
     var body: some View {
         VStack {
-            ForEach(random, id: \.self) {
-                Text($0)
-            }
-            
-            Button("Request permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            Button("Schedule notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "It looks hungry"
-                content.sound = UNNotificationSound.default
-
-                // show this notification five seconds from now
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                // choose a random identifier
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                // add our notification request
-                UNUserNotificationCenter.current().add(request)
-            }
+            Text("Hello")
         }
     }
 }
