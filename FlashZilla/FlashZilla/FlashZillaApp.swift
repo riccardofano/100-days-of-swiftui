7dc3ae45a66d36da8e3c75b1be9c8526d7df79e5
Riccardo Fano <rcr.fano@gmail.com>
2023-09-05 18:09:24 +0200

    Init FlashZilla app

---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Riccardo Fano on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
