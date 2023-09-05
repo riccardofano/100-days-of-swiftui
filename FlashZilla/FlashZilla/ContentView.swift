7c0840b099e62b3bda5c235e751b75d92faee744
Riccardo Fano <rcr.fano@gmail.com>
2023-09-05 18:34:05 +0200

    Explore more advanced gestures

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = 0.0;
+    @State private var finalAmount = 1.0;
+    
+    @State private var currentAngle = Angle.zero;
+    @State private var finalAngle = Angle.zero;
+    
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
+    
+    
     var body: some View {
-        VStack(spacing: 20) {
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+
+        // a combined gesture that forces the user to long press then drag
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        return VStack(spacing: 20) {
             Text("Tap")
                 .onTapGesture {
                     print("Tapped the button")
@@ -28,6 +61,55 @@ struct ContentView: View {
                 } onPressingChanged: { inProgress in
                     print("Is in progress: \(inProgress)")
                 }
+            Text("Magnification gesture")
+                .scaleEffect(finalAmount + currentAmount)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { amount in
+                            currentAmount = amount - 1
+                        }
+                        .onEnded { amount in
+                            finalAmount += currentAmount
+                            currentAmount = 0
+                        }
+                )
+            Text("Rotation gesture")
+                .rotationEffect(finalAngle + currentAngle)
+                .gesture(
+                    RotationGesture()
+                        .onChanged { angle in
+                            currentAngle = angle
+                        }
+                        .onEnded { angle in
+                            finalAngle += currentAngle
+                            currentAngle = .zero
+                        }
+                )
+            
+            VStack {
+                Text("Child")
+                    .onTapGesture {
+                        print("Text tapped")
+                    }
+            }
+            // Child usually has priority unless you specify a higher priority
+            .highPriorityGesture(
+            // simultaneous triggers both gestures
+            // .simultaneousGesture(
+                TapGesture()
+                    .onEnded { _ in
+                        print("VStack tapped")
+                    }
+            )
+            
+            // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+            Circle()
+                .fill(.red)
+                .frame(width: 64, height: 64)
+                .scaleEffect(isDragging ? 1.5 : 1)
+                .offset(offset)
+                .gesture(combined)
+            
         }
         .padding()
     }
