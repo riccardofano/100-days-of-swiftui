7dd19b82bb11512e4f902487acd3a4a3aa4ee13b
Riccardo Fano <rcr.fano@gmail.com>
2023-09-06 21:37:25 +0200

    Learn how Timer works

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -9,184 +9,19 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
-    @State private var currentAmount = 0.0;
-    @State private var finalAmount = 1.0;
-    
-    @State private var currentAngle = Angle.zero;
-    @State private var finalAngle = Angle.zero;
-    
-    // how far the circle has been dragged
-    @State private var offset = CGSize.zero
-    // whether it is currently being dragged or not
-    @State private var isDragging = false
-    
-    @State private var engine: CHHapticEngine?
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0;
     
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
+        Text("Hello world")
+            .onReceive(timer) { time in
+                if (counter == 5) {
+                    timer.upstream.connect().cancel()
+                    return
                 }
+                counter += 1;
+                print("\(time)")
             }
-
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-
-        // a combined gesture that forces the user to long press then drag
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        return VStack(spacing: 20) {
-            Text("Tap")
-                .onTapGesture {
-                    print("Tapped the button")
-                    simpleSuccess()
-                }
-            Text("Double tap")
-                .onTapGesture(count: 2) {
-                    print("Tapped the button twice")
-                    simpleError()
-                }
-            Text("Long press")
-                .onLongPressGesture {
-                    print("Long pressed the button")
-                    complexSuccess()
-                }
-            Text("Long long press")
-                .onLongPressGesture(minimumDuration: 2) {
-                    print("Long pressed for at least 2 seconds")
-                } onPressingChanged: { inProgress in
-                    print("Is in progress: \(inProgress)")
-                }
-            Text("Magnification gesture")
-                .scaleEffect(finalAmount + currentAmount)
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            currentAmount = amount - 1
-                        }
-                        .onEnded { amount in
-                            finalAmount += currentAmount
-                            currentAmount = 0
-                        }
-                )
-            Text("Rotation gesture")
-                .rotationEffect(finalAngle + currentAngle)
-                .gesture(
-                    RotationGesture()
-                        .onChanged { angle in
-                            currentAngle = angle
-                        }
-                        .onEnded { angle in
-                            finalAngle += currentAngle
-                            currentAngle = .zero
-                        }
-                )
-            
-            VStack {
-                Text("Child")
-                    .onTapGesture {
-                        print("Text tapped")
-                    }
-            }
-            // Child usually has priority unless you specify a higher priority
-            .highPriorityGesture(
-            // simultaneous triggers both gestures
-            // .simultaneousGesture(
-                TapGesture()
-                    .onEnded { _ in
-                        print("VStack tapped")
-                    }
-            )
-            
-            // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-            Circle()
-                .fill(.red)
-                .frame(width: 64, height: 64)
-                .scaleEffect(isDragging ? 1.5 : 1)
-                .offset(offset)
-                .gesture(combined)
-            
-            ZStack {
-                Rectangle()
-                    .fill(.blue)
-                    .frame(width: 200, height: 200)
-                    .onTapGesture {
-                        print("Rectangle tapped!")
-                    }
-
-                Circle()
-                    .fill(.red)
-                    .frame(width: 200, height: 200)
-                    // now the corners of the circle will also respond to taps
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        print("Circle tapped!")
-                    }
-                    // This is like pointer-events: none in css
-                    //.allowsHitTesting(false)
-            }
-            
-            VStack {
-                Text("Hello")
-                Spacer().frame(height: 100)
-                Text("World")
-            }
-            // this makes it so the spacer is also a tappable area
-            // .contentShape(Rectangle())
-            .onTapGesture {
-                print("VStack tapped!")
-            }
-        }
-        .padding()
-        .onAppear(perform: prepareHaptics)
-    }
-    
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
-    }
-    func simpleError() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.error)
-    }
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-    func complexSuccess() {
-        // make sure that the device supports haptics
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        // create one intense, sharp tap
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        events.append(event)
-
-        // convert those events into a pattern and play it immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
-        }
     }
 }
 
