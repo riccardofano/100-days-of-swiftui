7fd2b54b3a623355b71c50417b82db9168ea6e10
Riccardo Fano <rcr.fano@gmail.com>
2023-09-13 11:57:49 +0200

    Understand frames and coordinates inside GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,16 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            Text("Hello world")
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
+//        VStack {
+//            // `geo` contains the size the parent proposed
+//            GeometryReader { geo in
+//                Text("Hello, World!")
+//                    .frame(width: geo.size.width * 0.9, height: 40)
+//                    .background(.orange)
+//            }
+//            .background(.green)
+//
+//            Text("More text")
+//                .background(.bar)
+//        }
+    }
+}
+
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
                 .background(.green)
-                .position(x: 300, y: 100)
-                .background(.red)
-            
-            Text("Another hello")
-                .background(.blue)
-                .offset(x: 100, y: 100)
-                .background(.yellow)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
         }
     }
 }
