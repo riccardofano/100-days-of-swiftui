3c43ad8aaab443e99997fee47b524e0dca7a9297
Riccardo Fano <rcr.fano@gmail.com>
2023-09-13 11:11:12 +0200

    Init LayoutAndGeometry app

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Riccardo Fano on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
