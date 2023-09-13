eb318fdf118d0227d0be65ce7ec3335d47acfb1c
Riccardo Fano <rcr.fano@gmail.com>
2023-09-13 12:04:35 +0200

    Check out some cool effects using GeometryReaders

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,51 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
-//        VStack {
-//            // `geo` contains the size the parent proposed
-//            GeometryReader { geo in
-//                Text("Hello, World!")
-//                    .frame(width: geo.size.width * 0.9, height: 40)
-//                    .background(.orange)
-//            }
-//            .background(.green)
-//
-//            Text("More text")
-//                .background(.bar)
-//        }
-    }
-}
-
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("Top")
-            InnerView()
-                .background(.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            GeometryReader { geo in
-                Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+        ZStack {
+            GeometryReader { fullView in
+                ScrollView {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Text("Row #\(index)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .background(colors[index % 7])
+                                .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(height: 40)
                     }
+                }
             }
-            .background(.orange)
-            Text("Right")
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(1..<20) { num in
+                        GeometryReader { geo in
+                            Text("Number \(num)")
+                                .font(.largeTitle)
+                                .padding()
+                                .background(.red)
+                                .border(.black, width: 4)
+                                .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                                .frame(width: 200, height: 200)
+                        }
+                        .frame(width: 200, height: 200)
+                    }
+                }
+            }
         }
     }
 }
