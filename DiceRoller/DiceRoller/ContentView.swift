23e38b70b8789291139667dfe9581aabde139bed
Riccardo Fano <rcr.fano@gmail.com>
2023-09-15 10:40:15 +0200

    Add ability to roll dice

--- DiceRoller/DiceRoller/ContentView.swift
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -11,17 +11,36 @@ struct ContentView: View {
     let dieFaces = [4, 6, 8, 10, 12, 20, 100]
     @State private var selectedDie = 6
     
+    @State private var randomedValue = 6
+    
     var body: some View {
         NavigationView {
             Form {
-                Section("Number of faces") {
-                    Picker("", selection: $selectedDie) {
-                        ForEach(dieFaces, id: \.self) {
-                            Text("\($0)")
-                        }
+                Picker("Number of faces:", selection: $selectedDie) {
+                    ForEach(dieFaces, id: \.self) {
+                        Text("\($0)")
                     }
-                    .pickerStyle(.segmented)
                 }
+                
+                Button("Roll") {
+                    randomedValue = Int.random(in: 1..<selectedDie+1)
+                }
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(randomedValue)")
+                        .font(.largeTitle.bold())
+                        .frame(width: 100, height: 100)
+                        .background(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(.black, lineWidth: 5)
+                        )
+                        .padding(.top, 30)
+                    Spacer()
+                }
+                .listRowBackground(Color.white.opacity(0))
             }
             .navigationTitle("Dice roller")
         }
