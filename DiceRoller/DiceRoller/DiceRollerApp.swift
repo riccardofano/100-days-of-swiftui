2282743f90eb2d36d7e46aa73e4ffcfeb0d4b0ae
Riccardo Fano <rcr.fano@gmail.com>
2023-09-15 10:04:01 +0200

    Init DiceRoller milestone project

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Riccardo Fano on 2023-09-15.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
