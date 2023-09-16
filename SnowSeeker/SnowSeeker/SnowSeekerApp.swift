c38ec026c41fc0d58401676c8fa787f502cf352a
Riccardo Fano <rcr.fano@gmail.com>
2023-09-16 20:34:22 +0200

    Init SnowSeeker app

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Riccardo Fano on 2023-09-16.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
